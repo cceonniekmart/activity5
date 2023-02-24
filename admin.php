6e4a6edebed48284b4482d8e5a0892372ceee7ea
cceonniekmart <cceonniekmart@gmail.com>
2023-02-24 12:56:09 +0800

    fix on admin

--- admin.php
+++ admin.php
@@ -2,38 +2,46 @@
 session_start();
 include "db_conn.php";
 
+// check if user is logged in
 if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
-    $scores = array();
 
+    // initialize $Group variable
+    $Group = '';
+
+    // check if form is submitted
     if (isset($_POST['group'])) {
         $Group = $_POST['group'];
+    }
 
-        $sql = "SELECT answer_by, qstn1, qstn2 
+    // get scores for selected group
+    $sql = "SELECT answer_by, qstn1, qstn2 
             FROM tbl_answers 
             WHERE group_num='$Group'
             ORDER BY answer_by ASC";
 
-        $result = mysqli_query($conn, $sql);
-        if (!$result) {
-            die("Error: " . mysqli_error($conn));
-        }
+    $result = mysqli_query($conn, $sql);
+    if (!$result) {
+        die("Error: " . mysqli_error($conn));
+    }
 
-        // fetch rows and store scores for each user
-        while ($row = mysqli_fetch_assoc($result)) {
-            $answer_by = $row['answer_by'];
-            $question1_score = $row['qstn1'];
-            $question2_score = $row['qstn2'];
+    // fetch rows and store scores for each user
+    $scores = array();
+    while ($row = mysqli_fetch_assoc($result)) {
+        $answer_by = $row['answer_by'];
+        $question1_score = $row['qstn1'];
+        $question2_score = $row['qstn2'];
 
-            if (!isset($scores[$answer_by])) {
-                $scores[$answer_by] = array(
-                    'question1' => $question1_score,
-                    'question2' => $question2_score
-                );
-            } else {
-                $scores[$answer_by]['question1'] = $question1_score;
-                $scores[$answer_by]['question2'] = $question2_score;
-            }
+        if (!isset($scores[$answer_by])) {
+            $scores[$answer_by] = array(
+                'question1' => $question1_score,
+                'question2' => $question2_score
+            );
+        } else {
+            $scores[$answer_by]['question1'] = $question1_score;
+            $scores[$answer_by]['question2'] = $question2_score;
         }
+    }
+
 ?>
 
 <!DOCTYPE html>
@@ -49,33 +57,34 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
             <option value="Group1"<?php if ($Group == 'Group1') {echo ' selected';} ?>>Group 1</option>
             <option value="Group2"<?php if ($Group == 'Group2') {echo ' selected';} ?>>Group 2</option>
             <option value="Group3"<?php if ($Group == 'Group3') {echo ' selected';} ?>>Group 3</option>
-            <!-- add more options for additional users -->
+            <!-- add more options for additional groups -->
         </select>
         
         <button type="submit">Confirm</button>
     </form>
     
-    <table>
-        <thead>
-            <tr>
-                <th>User</th>
-                <th>Question 1</th>
-                <th>Question 2</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($scores as $user => $user_scores): ?>
+    <?php if (!empty($scores)): ?>
+        <table>
+            <thead>
                 <tr>
-                    <td><?php echo $user; ?></td>
-                    <td><?php echo $user_scores['question1']; ?></td>
-                    <td><?php echo $user_scores['question2']; ?></td>
+                    <th>User</th>
+                    <th>Question 1</th>
+                    <th>Question 2</th>
                 </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
-    <?php 
-    }
-    ?>
+            </thead>
+            <tbody>
+                <?php foreach ($scores as $user => $user_scores): ?>
+                    <tr>
+                        <td><?php echo $user; ?></td>
+                        <td><?php echo $user_scores['question1']; ?></td>
+                        <td><?php echo $user_scores['question2']; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php else: ?>
+        <p>Please select a group to view scores.</p>
+    <?php endif; ?>
 
 </body>
 </html>
@@ -83,7 +92,8 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 <?php 
     mysqli_close($conn); 
 } else {
+    // redirect to login page if user is not logged in
     header("Location: index.php");
     exit();
 }
-?> 
+?>
